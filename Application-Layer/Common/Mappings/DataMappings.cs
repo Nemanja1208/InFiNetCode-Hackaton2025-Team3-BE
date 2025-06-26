a54cc0164489e70c817614515ebb9366a5a8255e
Mohamad.N <m.neweder@gmail.com>
2025-06-26 14:09:11 +0200

    " error hantering kvar "

--- Application-Layer/Common/Mappings/DataMappings.cs
+++ Application-Layer/Common/Mappings/DataMappings.cs
@@ -3,6 +3,8 @@ using Application_Layer.IdeaSessions.DTOs;
 using AutoMapper;
 using Domain_Layer.Models;
 using Application_Layer.Steps.Dtos;
+using Application_Layer.IdeaSessions.Dto;
+using Application_Layer.IdeaSessions.Commands;
 
 namespace Application_Layer.Common.Mappings
 {
@@ -24,6 +26,15 @@ namespace Application_Layer.Common.Mappings
                 .ForMember(dest => dest.StepId, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Order, opt => opt.MapFrom(src => src.Order));
 
+            // Mappa från DTO till Command
+            CreateMap<CreateIdeaSessionDto, CreateIdeaSessionCommand>();
+
+            // Mappa från Command till entity
+            CreateMap<CreateIdeaSessionCommand, IdeaSession>();
+
+            // Mappa från entity till Output-DTO
+            CreateMap<IdeaSession, IdeaSessionDto>()
+            .ForMember(dest => dest.IdeaId, opt => opt.MapFrom(src => src.Id));
         }
     }
 }
