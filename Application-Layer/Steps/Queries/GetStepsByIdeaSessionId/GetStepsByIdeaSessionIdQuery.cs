a10a29688f620756eba23abb4e85f8dc737593cc
ilic-sara <mrssarailic@gmail.com>
2025-07-01 14:14:01 +0200

    Implemented functionality to get all steps for IdeaSession in order

---
+++ Application-Layer/Steps/Queries/GetStepsByIdeaSessionId/GetStepsByIdeaSessionIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Domain_Layer.Models;
+using MediatR;
+
+namespace Application_Layer.Steps.Queries.GetStepsByIdeaSessionId
+{
+    public record GetStepsByIdeaSessionIdQuery(Guid IdeaSessionId) : IRequest<OperationResult<List<StepResponseDto>>>;
+}
